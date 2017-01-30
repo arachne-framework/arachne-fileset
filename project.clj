3a92b4097257daf8d078d08fa41fc0d367be9636
Luke VanderHart <luke@vanderhart.net>
2017-01-30 14:33:17 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.arachne-framework/arachne-fileset "1.3.0"
+(defproject org.arachne-framework/arachne-fileset "1.4.0"
   :description "Tools for managing immutable filesets"
   :url "http://github.com/arachne-framework/arachne-fileset"
   :license {:name "Eclipse Public License"
